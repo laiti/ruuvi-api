210dc5dfcb5b64b9de2fb75a702c5c41acb1f1bf
Joonas Kokko <joonas@tulikukka.com>
2025-03-24 18:07:20 +0200

    Exported schema from DB.

--- schema.mysql.sql
+++ schema.mysql.sql
@@ -9,7 +9,7 @@ CREATE TABLE `history` (
   PRIMARY KEY (`id`),
   KEY `ruuvi_id` (`tag_id`),
   CONSTRAINT `history_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Create syntax for TABLE 'history_longterm'
 CREATE TABLE `history_longterm` (
@@ -19,8 +19,10 @@ CREATE TABLE `history_longterm` (
   `temperature_min` float DEFAULT NULL,
   `temperature_max` float DEFAULT NULL,
   `temperature_day_average` float DEFAULT NULL,
+  `temperature_night_average` float DEFAULT NULL,
   `humidity_min` float DEFAULT NULL,
   `humidity_max` float DEFAULT NULL,
+  `humidity_day_average` float DEFAULT NULL,
   `humidity_night_average` float DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `tag_id` (`tag_id`),
@@ -33,4 +35,4 @@ CREATE TABLE `tag` (
   `ruuvi_id` varchar(32) DEFAULT NULL,
   `name` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
